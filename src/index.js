314c2714c300396ede1949d2411925df541146bc
will greer <willfromspace@gmail.com>
2020-10-23 18:19:28 -0700

    cleanup/init starter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
+import App from './app'
 import { render } from 'react-dom';
-import 'normalize.css';
-import { GlobalStyles } from './global-styles';
-import { App } from './app';
-import { firebase } from './lib/firebase.prod';
-import { FirebaseContext } from './context/firebase';
+
 
 render(
-  <React.StrictMode>
-    <FirebaseContext.Provider value={{ firebase }}>
-      <GlobalStyles />
-      <App />
-    </FirebaseContext.Provider>
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
