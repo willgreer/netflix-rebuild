ac3aa6eba23041f2fcbae01cca7ac50952fe2fdc
will greer <willfromspace@gmail.com>
2020-11-23 16:11:16 -0700

    styled footer

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ import React from 'react';
 import jumboData from './fixtures/jumbo';
 import Jumbotron from './components/jumbotron';
 import { JumbotronContainer } from './containers/jumbotron';
+import { FaqsContainer } from './containers/faqs';
 
 export default function App() {
   return (
     <JumbotronContainer />
+    <FaqsContainer />
+    <FooterContainer />
   );
 }
---
+++ src/components/footer/styles/footer.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+	display: flex;
+	padding: 70px 56px;
+	margin: auto;
+	max-width: 1000px;
+	flex-direction: column;
+
+	@media (max-width: 1000px) {
+		padding: 70px 30px;
+	}
+`;
+
+export const Column = styled.div`
+	display: flex;
+	flex-direction: column;
+	text-align: left;
+`;
+
+export const Row = styled.div`
+	display: grid;
+	grid-template-columns: repeat(
+		auto-fill,
+		minmax(230px, 1fr)
+	);
+	grid-gap: 15px;
+
+	@media (max-width: 1000px) {
+		padding: 70px 30px;
+	}
+`;
+
+export const Link = styled.a`
+	color: #757575;
+	margin-bottom: 20px;
+	font-size: 13px;
+	text-decoration: none;
+`;
+
+export const Title = styled.p`
+	color: #757575;
+	font-size: 16px;
+	margin-bottom: 40px;
+`;
+
+export const Text = styled.p`
+	color: #757575;
+	font-size: 13px;
+	margin-bottom: 40px;
+`;
+
+export const Break = styled.p`
+	flex-basis: 100%;
+	height: 0;
+`;
